ccaf40d4a7d0421933fd0236c283ff41a458dcfe
UrielMaD <urielmd00@gmail.com>
2021-02-22 00:04:09 -0600

    Added arguments to resolvers

--- index.js
+++ index.js
@@ -1,25 +1,20 @@
 'use strict'
 
-const { buildSchema } = require('graphql')
+const { makeExecutableSchema } = require('graphql-tools')
 const express = require('express')
 const { graphqlHTTP } = require('express-graphql')
+const { readFileSync } = require('fs')
+const { join } = require('path')
+const resolvers = require('./lib/resolvers')
 
 const app = express()
 const port = process.env.port || 3000
 
 //Definiendo el esquema
-const schema = buildSchema(`
-    type Query {
-        hello: String
-    }
-`)
-
-// Configurar los resolvers
-const resolvers = {
-    hello: () => {
-        return 'Hola Mundo'
-    }
-}
+const typeDefs = readFileSync(
+    join(__dirname, 'lib', 'schema.graphql'), 'utf-8'
+)
+const schema = makeExecutableSchema({ typeDefs, resolvers })
 
 app.use('/api', graphqlHTTP({
     schema: schema,
